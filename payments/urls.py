e4ffac40229aaa86d7199aa4e37bf744c657a3f4
mmunson94 <michaeljackmunson9994@outlook.com>
2024-01-16 19:38:37 +0000

    Redirect user properly on payment made

--- payments/urls.py
+++ payments/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.HomePageView.as_view(), name='home'),
     path('config/', views.stripe_config, name='stripe_config'),
     path('create-checkout-session/', views.create_checkout_session, name='create_checkout_session'),
+    path('success/', views.SuccessView.as_view(), name='success'),
+    path('cancelled/', views.CancelledView.as_view(), name='cancelled'),
 ]
